4266940005662c48b6a7c248e4a3e8294539b5a3
Michael Kosiorek <97412062+michaelkosiorek@users.noreply.github.com>
2024-03-30 23:21:30 -0400

    Normalization algs

--- normalization_algs.cpp
+++ normalization_algs.cpp
@@ -2,9 +2,27 @@
 
 
 bool detect_negative_one_edge(double edge_cost, int n) {
-    
+    double check = 3 * (n + 1) * get_fractional_part(edge_cost);
+    if (2*n >= check && check >= 1) return true;
+    else return false;
 }
 
+bool detect_positive_one_edge(double edge_cost, int n) {
+    double check = truncate(edge_cost/3*(n+1));
+    if (2*n >= check && check >= 1) return true;
+    else return false;
+}
 
-bool detect_positive_one_edge(double edge_cost, int n);
-bool detect_zero_edge(double edge_cost, int n);
+bool detect_zero_edge(double edge_cost, int n) {
+    double check = truncate(edge_cost) % (3*(n+1));
+    if (3*n >= check && check >= 0) return true;
+    else return false;
+}
+
+double get_fractional_part(double edge_cost) {
+    return static_cast<double>(edge_cost-truncate(edge_cost));
+}
+
+int truncate(double edge_cost) {
+    return static_cast<int>(floor(edge_cost));
+}
--- normalization_algs.h
+++ normalization_algs.h
@@ -1,8 +1,12 @@
 #ifndef _NORMALIZATION_ALGS_H_
 #define _NORMALIZATION_ALGS_H_
 
+#include <cmath>
+
 bool detect_negative_one_edge(double edge_cost, int n);
 bool detect_positive_one_edge(double edge_cost, int n);
 bool detect_zero_edge(double edge_cost, int n);
+double get_fractional_part(double edge_cost);
+int truncate(double edge_cost);
 
 #endif
